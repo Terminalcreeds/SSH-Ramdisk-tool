d58d25e1a20e834cfe81e0c14b62ee49da6fdc0b
Dr238 <100372602+Dr238@users.noreply.github.com>
2022-03-11 22:01:50 +0100

    Update restored_external.cc

--- Stuff/restored_external.cc
+++ Stuff/restored_external.cc
@@ -13,11 +13,11 @@ int main(int argc, char const *argv[]) {
   std::cout << THICKRED << "@    @@    @@@@     @@@@   @    @     @@@@   @   @  @@@@@@   @@@@             " << RESET << std::endl;
   std::cout << THICKRED << "                                                                         @    " << RESET << std::endl << std::endl;
   int pid, i;
-   char *arg[] = {"mount_apfs", "/dev/disk0s1", "/mnt1", NULL}; // /dev/disk0s1s1 == rootfs
+   char *arg[] = {"mount_apfs", "/dev/disk0s1s1", "/mnt1", NULL}; // /dev/disk0s1s1 == rootfs
    posix_spawn(&pid, "/System/Library/Filesystems/apfs.fs/mount_apfs", NULL, NULL, (char* const*)arg, NULL);
    waitpid(pid, &i, 0);
    //
-   char *arg3[] = {"bash","-c","/System/Library/Filesystems/apfs.fs/mount_apfs /dev/disk0s2 /mnt2", NULL}; // /private/var is mounted on this one
+   char *arg3[] = {"bash","-c","/System/Library/Filesystems/apfs.fs/mount_apfs /dev/disk0s1s2 /mnt2", NULL}; // /private/var is mounted on this one
    posix_spawn(&pid, "/bin/bash",NULL, NULL, (char* const*)arg3, NULL);
    waitpid(pid, &i, 0);
    sleep(1);
@@ -26,10 +26,6 @@ int main(int argc, char const *argv[]) {
    posix_spawn(&pid, "/usr/local/bin/restored_external_original", NULL, NULL, (char* const*)arg4, NULL);
    waitpid(pid, &i, 0);
    sleep(1);
-   //
-   char *arg2[] = {"launchctl", "load", "-w", "/Library/LaunchDaemons/com.openssh.sshd.plist", NULL};
-   posix_spawn(&pid, "/bin/launchctl", NULL, NULL, (char* const*)arg2, NULL);
-   waitpid(pid, &i, 0);
    sleep(99999);
   return 0;
 }
